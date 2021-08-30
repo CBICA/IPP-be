6a0cc3b7657cd26d9d961fbc085adde0f368dbfc
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2021-08-30 00:54:35 -0400

    small changes

--- shell/qsub.sh
+++ shell/qsub.sh
@@ -2,14 +2,19 @@
 
 set -euf -o pipefail
 
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <local_port> <remote_port> <ssh_address>"
+    exit 1
+fi
+
 cd $(dirname $0) # we start in the directory of the script
 root_dir=$PWD
-INPUT_DIR=$root_dir/inputs # where to place downloaded files
+INPUT_DIR=$root_dir/inputs   # where to place downloaded files
 OUTPUT_DIR=$root_dir/outputs # experdir
 # setup ssh tunnel
-LOCAL_PORT=35094
-REMOTE_PORT=5000
-SSH_ADDR=root@localhost
+LOCAL_PORT=$1
+REMOTE_PORT=$2
+SSH_ADDR=$3
 API_URL="http://localhost:${LOCAL_PORT}/experiments"
 ssh -qnNT -L ${LOCAL_PORT}:127.0.0.1:${REMOTE_PORT} ${SSH_ADDR} &
 pid=$!
@@ -71,11 +76,13 @@ for experiment in $queue; do
         command="$command $(printf '%s %s' $key $value)"
     done
     cd $experdir
-    jid=$(qsub -cwd -b y -terse -o output.txt $(echo $app | jq -r '.SGEJobResources') $command)
+    resources=$(echo $app | jq -r '.SGEJobResources')
+    set +x
+    jid=$(qsub -cwd -b y -terse -o output.txt $resources $command)
+    set -x
     cd $root_dir
-    echo $jid >> jids
-    echo "$jid $eid" >> jid_eid
+    echo "$jid $eid" >>jids
 
 done
 
-kill -9 ${pid}
+kill -9 $pid
